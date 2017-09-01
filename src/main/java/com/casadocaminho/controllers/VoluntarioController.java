ca1eceb0737faaff71a362c4d68e0b97b280903a
Lucas William Marchi Silva <lssi@gft.com>
2017-09-01 16:17:24 -0300

    Arrumando problemas com POST

--- src/main/java/com/casadocaminho/controllers/VoluntarioController.java
+++ src/main/java/com/casadocaminho/controllers/VoluntarioController.java
@@ -2,9 +2,9 @@ package com.casadocaminho.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.casadocaminho.models.Voluntario;
 import com.casadocaminho.repositories.VoluntarioRepository;
@@ -22,15 +22,16 @@ public class VoluntarioController {
 	}
 	
 	@RequestMapping("/cadastrar")
-	public String cadastrar(Voluntario voluntario) {
+	public ModelAndView cadastrar(Voluntario voluntario) {
 		voluntarioRepository.save(voluntario);
-		return "/voluntario/lista_voluntarios";
+		return new ModelAndView("redirect:/voluntario");
 	}
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar(Model model) {
-		model.addAttribute("voluntarios", voluntarioRepository.findAll());
-		return "/voluntario/lista_voluntarios";
+	@RequestMapping(value="/buscar", method=RequestMethod.GET)
+	public ModelAndView listar() {
+		ModelAndView mv = new ModelAndView("/voluntario/lista_voluntarios");
+		mv.addObject("voluntarios", voluntarioRepository.findAll());
+		return mv;
 	}
 	
 }
